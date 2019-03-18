130105b933721cd1b4e102a4ce88808387f34e25
Trevor <tlarnold10@hotmail.com>
2019-03-17 21:52:40 -0500

    adding a pong tourney app, add player

---
+++ addPongPlayer.pyw
@@ -0,0 +1,34 @@
+import sys, psycopg2
+from PyQt5.QtWidgets import QDialog, QApplication
+from pongPlayerAdd import *
+class MyForm(QDialog):
+	def __init__(self):
+		super().__init__()
+		self.ui = Ui_Dialog()
+		self.ui.setupUi(self)
+		self.ui.pushButton.clicked.connect(self.addPlayer)
+		self.show()
+	def addPlayer(self):
+		conn = psycopg2.connect(host="localhost",database="arnold",user="postgres",password="snowlep")
+		curs = conn.cursor()
+		insertFirstName = self.ui.lineEdit.text()
+		insertLastName = self.ui.lineEdit_2.text()
+		query = """
+			INSERT INTO players 
+			(first_name, last_name) 
+			VALUES ('%s', '%s')
+		""" % (insertFirstName, insertLastName)
+		curs.execute(query)
+		conn.commit()
+		conn.close()
+
+		self.ui.label.setText("User Created, please add.")
+		self.ui.lineEdit.setText("")
+		self.ui.lineEdit_2.setText("")
+
+
+if __name__=="__main__":
+	app = QApplication(sys.argv)
+	w = MyForm()
+	w.show()
+	sys.exit(app.exec_())
---
+++ pongPlayerAdd.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'pongPlayerAdd.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(590, 381)
+        self.verticalLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(50, 50, 481, 271))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label_3 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout_2.addWidget(self.label_3)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.horizontalLayout_2.addWidget(self.lineEdit_2)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label_2 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout.addWidget(self.label_2)
+        self.lineEdit = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout.addWidget(self.lineEdit)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Add A Persion"))
+        self.label_3.setText(_translate("Dialog", "Last Name"))
+        self.label_2.setText(_translate("Dialog", "First Name"))
+        self.pushButton.setText(_translate("Dialog", "Add Player"))
+
